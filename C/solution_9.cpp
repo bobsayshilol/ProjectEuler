b0fa92c3b83b090f280842f49420d02524d6126d
bobsayshilol <bobsayshilol@live.co.uk>
2016-01-31 17:15:23 +0000

    Make solution 100x faster by using some more maths.

--- C/solution_9.cpp
+++ C/solution_9.cpp
@@ -16,8 +16,14 @@
  * Also b < 500, since b >= 500 => c > b >= 500 => a + b + c >= a + 500 + 500 = a + 1000 > 1000
  * 
  * So we are looking for a solution to ab - 1000(a + b) + 500000 = 0 with a < b < 500
- * Note that there is probably some optimisation that can be done by treating ^ as a continuous function
- * and guessing a first approximation.
+ * 
+ * Solving for a gives
+ * a = 1000 * (500 - b) / (1000 - b)
+ * and similarly for b
+ * b = 1000 * (500 - a) / (1000 - a)
+ * 
+ * So we are looking for an integer b < 500 st 1000 * (500 - b) is divisible by (1000 - b)
+ * 
  * 
  * (x-a)(x-b) = x^2 - x(a+b) + ab, but 1000^2 != 500000, so that's of no use
  */
@@ -32,16 +38,24 @@
 // Begin the solution
 SOLUTION_BEGIN(9, uint64_t)
 {
-	// Loop within a loop to look for the right values
+	// Go through the numbers as described above
 	for (uint64_t b=1; b<500; b++)
 	{
-		for (uint64_t a=1; a<b; a++)
+		// Calculate the numerator and denominator
+		const uint64_t a_numerator = 500000 - 1000 * b;
+		const uint64_t a_denominator = 1000 - b;
+		
+		// See if it's divisible
+		if ( (a_numerator % a_denominator) == 0 )
 		{
-			if (a*b - 1000 * (a + b) + 500000 == 0)
-			{
-				// Calculate abc
-				result = (1000 - a - b) * (1000 * (a + b) - 500000);
-			}
+			// Calculate a
+			uint64_t a = a_numerator / a_denominator;
+			
+			// Calculate abc = (1000 - a - b)(1000(a + b) - 500000)
+			result = (1000 - a - b) * (1000 * (a + b) - 500000);
+			
+			// Finished
+			break;
 		}
 	}
 }
