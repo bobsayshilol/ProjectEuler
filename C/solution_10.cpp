9490b60b83531a4437304b4db18aede5978b9574
bobsayshilol <bobsayshilol@live.co.uk>
2016-02-07 01:00:53 +0000

    Make solution 10 twice as fast by removing unnecessary calculations.

--- C/solution_10.cpp
+++ C/solution_10.cpp
@@ -29,14 +29,19 @@ SOLUTION_BEGIN(10, uint64_t)
 	// Use a uint8_t rather than a bool since the bool specialisation is slooooow
 	std::vector<uint8_t> sieve(N, 1);
 	
-	// Create the sieve
-	for (uint64_t prime=2; prime<N; prime++)
+	// Treat 2 as a special case
+	result += 2;
+	
+	// Create the sieve.
+	// We can skip all the even numbers since we know that 2 will be a factor
+	for (uint64_t prime=3; prime<N; prime+=2)
 	{
 		// If this value hasn't been set then it must be prime
 		if (sieve[prime] == 1)
 		{
-			// Go through all multiples of prime and set them to false in the sieve
-			for (uint64_t multiple=2*prime; multiple<N; multiple+=prime)
+			// Go through all multiples of prime and set them to false in the sieve.
+			// Since prime is guaranteed to be odd we need to add 2*prime each time
+			for (uint64_t multiple=prime*prime; multiple<N; multiple+=2*prime)
 			{
 				sieve[multiple] = 0;
 			}
