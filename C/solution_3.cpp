5a9557e5b8b43db9259f6b20b7e1890f93c84639
bobsayshilol <bobsayshilol@live.co.uk>
2016-02-07 01:07:56 +0000

    Make solution 3 almost twice as fast by doing the same thing we did to solution 10.

--- C/solution_3.cpp
+++ C/solution_3.cpp
@@ -29,14 +29,20 @@ SOLUTION_BEGIN(3, uint64_t)
 	// Use a uint8_t rather than a bool since the bool specialisation is slooooow
 	std::vector<uint8_t> sieve(sqrtN+1, 1);
 	
+	// Special case 2 so we can speed up the loops below
+	if ((N % 2) == 0)
+	{
+		result = 2;
+	}
+	
 	// Create the sieve
-	for (uint64_t prime=2; prime<=sqrtN; prime++)
+	for (uint64_t prime=3; prime<=sqrtN; prime+=2)
 	{
 		// If this value hasn't been set then it must be prime
 		if (sieve[prime] == 1)
 		{
 			// Go through all multiples of prime and set them to false in the sieve
-			for (uint64_t multiple=2*prime; multiple<=sqrtN; multiple+=prime)
+			for (uint64_t multiple=prime*prime; multiple<=sqrtN; multiple+=2*prime)
 			{
 				sieve[multiple] = 0;
 			}
