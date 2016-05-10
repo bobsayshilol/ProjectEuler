9c0fa832b71c9bfaa5155635d264c3d484bc0ca7
bobsayshilol <bobsayshilol@live.co.uk>
2016-05-10 01:00:09 +0100

    Move the solution list out of common.h so we don't have to recompile everything whenever we add a new one.

--- C/main.cpp
+++ C/main.cpp
@@ -4,6 +4,46 @@
 
 
 /**
+ * A list of solved problems for producing lists at compile time
+ * Each macro must be of the form
+ *   macro(problem_number, return_type, expected_result)
+ * The results are taken from projecteuler-solutions
+ */
+#define SOLUTION_LIST(custom_macro) \
+	custom_macro(1, uint64_t, 233168) \
+	custom_macro(2, uint64_t, 4613732) \
+	custom_macro(3, uint64_t, 6857) \
+	custom_macro(4, uint64_t, 906609) \
+	custom_macro(5, uint64_t, 232792560) \
+	custom_macro(6, uint64_t, 25164150) \
+	custom_macro(7, uint64_t, 104743) \
+	custom_macro(8, uint64_t, 23514624000) \
+	custom_macro(9, uint64_t, 31875000) \
+	custom_macro(10, uint64_t, 142913828922) \
+	custom_macro(11, uint64_t, 70600674) \
+	custom_macro(12, uint64_t, 76576500) \
+	custom_macro(13, uint64_t, 5537376230) \
+	custom_macro(14, uint64_t, 837799) \
+	custom_macro(15, uint64_t, 137846528820) \
+	custom_macro(16, uint64_t, 1366) \
+	custom_macro(17, uint64_t, 21124) \
+	custom_macro(18, uint64_t, 1074) \
+	custom_macro(19, uint64_t, 171) \
+	custom_macro(20, uint64_t, 648) \
+	custom_macro(21, uint64_t, 31626) \
+	custom_macro(22, uint64_t, 871198282) \
+	custom_macro(23, uint64_t, 4179871) \
+
+
+
+// Declare each function for the linker.
+// This could just go in main.cpp...
+#define SOLUTION_DECLARATION(problem_number, return_type, ...) SOLUTION_ENTRY(problem_number, return_type) ;
+SOLUTION_LIST( SOLUTION_DECLARATION )
+
+
+
+/**
  * Timers for timing each solution
  */
 #include <chrono>
